ab02f4a45a7d7b47e2b1d61889b95e878a0331a3
Fix3d <serhanduzce@gmail.com>
2023-05-06 03:18:07 +0300

    new fishing baits

--- src/main/java/codes/biscuit/skyblockaddons/features/BaitManager.java
+++ src/main/java/codes/biscuit/skyblockaddons/features/BaitManager.java
@@ -63,15 +63,22 @@ public class BaitManager {
 
         EntityPlayerSP player = Minecraft.getMinecraft().thePlayer;
         for (ItemStack itemStack : player.inventory.mainInventory) {
+            BaitType bait;
+
+            // Glowy Chum and Hot bait doesn't contain skyblock id
             String skyblockID = ItemUtils.getSkyblockItemID(itemStack);
-            if (skyblockID == null) {
-                continue;
+            if (skyblockID != null)
+                bait = BaitType.getByItemID(skyblockID);
+            else {
+                String skullOwnerID = ItemUtils.getSkullOwnerID(itemStack);
+                if (skullOwnerID != null)
+                    bait = BaitType.getBySkullID(skullOwnerID);
+                else
+                    continue;
             }
 
-            BaitType bait = BaitType.getByItemID(skyblockID);
-            if (bait == null) {
+            if (bait == null)
                 continue;
-            }
 
             baitsInInventory.put(bait, baitsInInventory.getOrDefault(bait, 0) + itemStack.stackSize);
         }
@@ -90,25 +97,45 @@ public class BaitManager {
         BLESSED("§aBlessed Bait", "BLESSED_BAIT", "54b94885-521b-3700-a138-31330f0aba0d", "b060a46497cee7611346c9dd8eb8f5de4483de259147211353848356de15d19"),
         WHALE("§9Whale Bait", "WHALE_BAIT", "d88b7890-b977-3de5-acc6-5e14b17cc1f3", "33aa71676e81fb53a040dfdca3e5b47d53e6efd665e69fb439778e8c4efb1cc"),
         ICE("§aIce Bait", "ICE_BAIT", "b2b19dcd-dc67-31df-a790-e6cf07ae12ac", "11136616d8c4a87a54ce78a97b551610c2b2c8f6d410bc38b858f974b113b208"),
-        SHARK("§aShark Bait", "SHARK_BAIT", "9a6c7271-a12d-3941-914c-7c456a086c5a", "edff904124efe486b3a54261dbb8072b0a4e11615ad8d7394d814e0e8c8ef9eb")
+        SHARK("§aShark Bait", "SHARK_BAIT", "9a6c7271-a12d-3941-914c-7c456a086c5a", "edff904124efe486b3a54261dbb8072b0a4e11615ad8d7394d814e0e8c8ef9eb"),
+        CORRUPTED("§fCorrupted Bait", "CORRUPTED_BAIT", "e4785c0e-3c90-3af3-9ac8-c8c49653af4f", "4bbcddd45cd347865bceab3e3dc5d382723463963f85ecce81cdd61b53db14e4"),
+        GLOWY_CHUM("§aGlowy Chum Bait", "GLOWY_CHUM_BAIT", "aed1035d-0ce1-35e6-b532-8c26a596f510", "dfdc1eed684dd805eae96d132e3da53d64267d7361388d5e2c67f5969871e71d"),
+        HOT("§aHot Bait", "HOT_BAIT", "e1eada20-e1b5-39ee-8dcb-b527054e33e3", "213c6899d97109c6cacbbcdd01e8900abaf46432f197595baa15ad137d5fb9ba"),
+        OBFUSCATED_FISH_1("§f§kObfuscated 1", "OBFUSCATED_FISH_1", "6044c06f-fb6e-3de8-9e7f-485b0edfdf3a", "e1f4d91e1bf8d3c4258fe0f28ec2fa40670e25ba06ac4b5cb1abf52a83731a9c"),
+        OBFUSCATED_FISH_2("§a§kObfuscated 2", "OBFUSCATED_FISH_2", "53807fee-2e39-3230-bdd1-3efcf0be73a9", "4631953a0351988029b90e838181e4e563d782e470ea33b8c612756f730625c2"),
+        OBFUSCATED_FISH_3("§9§kObfuscated 3", "OBFUSCATED_FISH_3", "c3701fb3-bd25-3772-b684-8572068672ff", "3c800c71b925587213382eeaaa426ed63112503e278ff9f5b3d39dbdb95d31d0")
         ;
 
         private final String itemID;
+        private final String skullID;
         private final ItemStack itemStack;
 
         BaitType(String name, String itemID, String skullID, String textureURL) {
             this.itemID = itemID;
+            this.skullID = skullID;
             this.itemStack = ItemUtils.createSkullItemStack(name, itemID, skullID, textureURL);
         }
 
         /**
          * Check to see if the given name matches a bait's skyblock item ID
-         *
          * @return The matching BaitType or null
          */
         public static BaitType getByItemID(String itemID) {
             for (BaitType bait : values()) {
                 if (itemID.startsWith(bait.itemID)) {
+                    return bait;
+                }
+            }
+            return null;
+        }
+
+        /**
+         * Check to see if the given id matches a bait's skull owner ID
+         * @return The matching BaitType or null
+         */
+        public static BaitType getBySkullID(String skullID) {
+            for (BaitType bait : values()) {
+                if (skullID.equals(bait.skullID)) {
                     return bait;
                 }
             }
