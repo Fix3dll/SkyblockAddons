e04600e91606f4dcffe53129a5f1187de764a57e
Fix3d <serhanduzce@gmail.com>
2024-04-12 14:23:11 +0300

    remove unused

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -16,9 +16,6 @@ pluginManagement {
             }
         }
     }
-    plugins {
-        kotlin("jvm") version "1.9.23"
-    }
 }
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version("0.8.0")
