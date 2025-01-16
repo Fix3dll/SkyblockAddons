ebb0c30c7a3c45da41e266b2848b2ff88efb6c73
Fix3d <contact@fix3dll.com>
2025-01-16 22:16:57 +0300

    add: VanillaHUD compat

---
+++ src/main/java/codes/biscuit/skyblockaddons/mixins/transformers/VanillaHUDMixin.java
@@ -0,0 +1,25 @@
+package codes.biscuit.skyblockaddons.mixins.transformers;
+
+import codes.biscuit.skyblockaddons.SkyblockAddons;
+import codes.biscuit.skyblockaddons.core.feature.Feature;
+import codes.biscuit.skyblockaddons.features.tablist.TabListParser;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Pseudo;
+
+@Pseudo
+@Mixin(targets = "org.polyfrost.vanillahud.VanillaHUD")
+public class VanillaHUDMixin {
+
+    /**
+     * @author Fix3dll
+     * @reason VanillaHUD legacy SBA compatibility
+     */
+    @Overwrite
+    private static boolean isSBACompactTab() {
+        return SkyblockAddons.getInstance().getUtils().isOnSkyblock()
+                && Feature.COMPACT_TAB_LIST.isEnabled()
+                && TabListParser.getRenderColumns() != null;
+    }
+
+}
