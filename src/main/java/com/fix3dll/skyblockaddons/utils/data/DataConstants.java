386f52504fa812022e74696203006f2ff5cc3e58
Fix3d <contact@fix3dll.com>
2025-07-02 22:09:22 +0300

    revert: use latest tag for data constants again

--- src/main/java/com/fix3dll/skyblockaddons/utils/data/DataConstants.java
+++ src/main/java/com/fix3dll/skyblockaddons/utils/data/DataConstants.java
@@ -7,7 +7,7 @@ package com.fix3dll.skyblockaddons.utils.data;
  */
 class DataConstants {
 
-    static final String CDN_BASE_URL = "https://cdn.jsdelivr.net/gh/Fix3dll/SkyblockAddons-Data@data/fabric/";
-    static final String FALLBACK_CDN_BASE_URL = "https://fastly.jsdelivr.net/gh/Fix3dll/SkyblockAddons-Data@data/fabric/";
+    static final String CDN_BASE_URL = "https://cdn.jsdelivr.net/gh/Fix3dll/SkyblockAddons-Data@latest/fabric/";
+    static final String FALLBACK_CDN_BASE_URL = "https://fastly.jsdelivr.net/gh/Fix3dll/SkyblockAddons-Data@latest/fabric/";
 
 }
