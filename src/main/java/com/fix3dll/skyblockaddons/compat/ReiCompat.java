74f3178cb795dcadcfd934361f4dba3b858409af
Fix3d <contact@fix3dll.com>
2026-03-03 03:46:54 +0300

    fix: static empty list instead of creating a new one

--- src/main/java/com/fix3dll/skyblockaddons/compat/ReiCompat.java
+++ src/main/java/com/fix3dll/skyblockaddons/compat/ReiCompat.java
@@ -7,8 +7,11 @@ import me.shedaniel.rei.api.client.registry.screen.ExclusionZones;
 import net.minecraft.client.gui.screens.inventory.ContainerScreen;
 
 import java.util.Collections;
+import java.util.List;
 
 public class ReiCompat implements REIClientPlugin {
+
+    private static final List<Rectangle> EMPTY = List.of();
 
     @Override
     public void registerExclusionZones(ExclusionZones zones) {
@@ -18,7 +21,7 @@ public class ReiCompat implements REIClientPlugin {
                     if (ScreenHook.islandWarpGui != null) {
                         return Collections.singleton(new Rectangle(0, 0, screen.width, screen.height));
                     }
-                    return Collections.emptyList();
+                    return EMPTY;
                 }
         );
     }
