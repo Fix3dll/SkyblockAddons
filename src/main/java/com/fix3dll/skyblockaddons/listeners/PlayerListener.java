bf2a0f227dacc34c85874c9f8e95fdcf8d1d400a
Fix3d <serhanduzce@gmail.com>
2026-03-20 06:11:46 +0300

    fix: always show level 1 pet price in custom item resolution

--- src/main/java/com/fix3dll/skyblockaddons/listeners/PlayerListener.java
+++ src/main/java/com/fix3dll/skyblockaddons/listeners/PlayerListener.java
@@ -1703,12 +1703,6 @@ public class PlayerListener {
                             .replace(" ", "_")
                             .replace("_EGG", "");
                     apiItemId = nameGroup + ";" + rarity.ordinal();
-
-                    int petLevel = Integer.parseInt(m.group("level"));
-                    if (petLevel != 0 && petLevel % 100 == 0) {
-                        extraString = "+" + petLevel;
-                        apiItemId += extraString;
-                    }
                 }
             }
             default -> {
