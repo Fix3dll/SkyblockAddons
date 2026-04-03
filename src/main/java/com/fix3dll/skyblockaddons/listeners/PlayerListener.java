b6d6315a72b29896e07be1e69e66d6597af4dbba
Fix3d <serhanduzce@gmail.com>
2026-04-03 20:13:30 +0300

    fix: prevent unintended current pet update when parsing pet ItemStack

--- src/main/java/com/fix3dll/skyblockaddons/listeners/PlayerListener.java
+++ src/main/java/com/fix3dll/skyblockaddons/listeners/PlayerListener.java
@@ -1648,7 +1648,7 @@ public class PlayerListener {
 
         switch (apiItemId) {
             case "PET" -> {
-                PetManager.Pet pet = PetManager.getInstance().getPetFromItemStack(itemStack);
+                PetManager.Pet pet = PetManager.getInstance().getPetFromItemStack(itemStack, false);
 
                 if (pet != null) {
                     PetInfo petInfo = pet.getPetInfo();
