2045d9a1314bb2162a2f86ad9d0aecf2f72829df
Fix3dll <contact@fix3dll.com>
2026-07-12 13:10:15 +0300

    fix: wrong pressure icon

--- src/main/java/com/fix3dll/skyblockaddons/listeners/RenderListener.java
+++ src/main/java/com/fix3dll/skyblockaddons/listeners/RenderListener.java
@@ -1140,7 +1140,7 @@ public class RenderListener {
                 if (pressure == -1) return;
                 boolean icon = feature.isEnabled(FeatureSetting.PRESSURE_TEXT_ICON);
                 boolean lastRemembered = main.getPlayerListener().getActionBarParser().isUseLastRememberedPressure();
-                text = (icon ? "\uE008" : "") + (lastRemembered ? "~" : "") + TextUtils.formatNumber(pressure) + "%";
+                text = (icon ? "\uE01B" : "") + (lastRemembered ? "~" : "") + TextUtils.formatNumber(pressure) + "%";
             }
             default -> {
                 return;
